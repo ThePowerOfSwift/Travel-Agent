6181bde2343b4258c742a07c8adef3d10aa58c18
obada <obada@10.0.2.15>
2018-11-21 18:40:00 -0800

    last20/11

---
+++ travel/v/changepass.swift
@@ -0,0 +1,30 @@
+//
+//  changepass.swift
+//  travel
+//
+//  Created by obada on 11/20/18.
+//  Copyright © 2018 Abdo. All rights reserved.
+//
+
+import UIKit
+
+class changepass: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
